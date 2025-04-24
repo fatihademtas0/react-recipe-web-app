031cf4cfe2716db94969a35043a2489df12064bb
fatihademtas0 <fatihademtas@outlook.com>
2025-04-24 13:20:40 +0300

    adding ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Main() {
+    const [ingredients, setIngredients] = useState([]);
+
+    const [inputValue, setInputValue] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (inputValue.trim() === "") return;
+
+        setIngredients((prev) => [...prev, inputValue]);
+        setInputValue("");
+    }
+
     return (
         <main>
-            <form className='add-ingredient-form'>
+            <form onSubmit={handleSubmit} className='add-ingredient-form'>
                 <input
                     aria-label='Add ingredient'
                     type='text'
                     placeholder='e.g. avacado'
+                    name='ingredient'
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
                 />
 
                 <button>Add ingredient</button>
             </form>
+
+            <ul>
+                {ingredients &&
+                    ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                    ))}
+            </ul>
         </main>
     );
 }
